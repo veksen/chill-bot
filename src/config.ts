7c0c506244fd60403da99ede23cf652c864664c4
Jean-Philippe Sirois <jp@veksen.net>
2019-04-03 03:59:56 -0400

    chore: extract Atlas DB to env

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const TOKEN: string | void = process.env.TOKEN;
 
 export const PREFIX: string = "/";
 
-const { ATLAS_USERNAME, ATLAS_PASSWORD } = process.env;
+const { ATLAS_USERNAME, ATLAS_PASSWORD, ATLAS_DB } = process.env;
 
 // TODO: possibly extract to env
-export const MONGO_PATH: string = `mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@chill-bot-le33j.mongodb.net/test?retryWrites=true`;
+export const MONGO_PATH: string = `mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@${ATLAS_DB}?retryWrites=true`;
